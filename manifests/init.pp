3978fb5b1d888183e5f9b7a81775bb7b2f40e6a7
Ugo Paternostro <ugo.git@paternostro.org>
2016-02-25 23:06:56 +0100

    Recover parameter handling from master

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,11 @@ class nullmailer (
   $absentpackages = $nullmailer::params::absentpackages,
   $service = $nullmailer::params::service,
   $manage_etc_mailname = $nullmailer::params::manage_etc_mailname,
-  $ensure = 'present') inherits nullmailer::params {
+  $ensure = 'present',
+  $adminaddr = "root@$::domain",
+  $remoterelay = "smtp.$::domain",
+  $remoteopts = ''
+) inherits nullmailer::params {
   if $ensure == 'present' {
     anchor { 'nullmailer::start': } ->
     class { 'nullmailer::package': } ~>
